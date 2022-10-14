aa52072365b3c0ec12dbf238ef51525fd7ec63d0
MixailLOL <medvedztel7@gmail.com>
2022-10-14 14:34:08 +0300

    cool click effect; array in state

--- src/index.js
+++ src/index.js
@@ -21,24 +21,29 @@ class Game extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            colors_number: 0,
-            rgb_array : [100,0,0],
+            colors_id: [0],
+            rgb_array : [[]],
         };
         this.get_random_color= this.get_random_color.bind(this);
     }
 
-    get_random_color(){
+    get_random_color(id){
         var color =  arrayRandElement(colors_data)
         var numbers = get_numbers_from_text(color[1])
-        console.log(numbers)
-        this.setState({rgb_array: numbers});
+        this.setState(previousState => ({
+            rgb_array: [...previousState.rgb_array, numbers],
+            colors_id:[...previousState.colors_id, previousState.colors_id[previousState.colors_id.length-1]+1]
+        }));
+       
     }
 
 
     colors_to_choice(){
         return(
             <div id="colors_to_choice" class="h-2/3 w-full absolute">
-                <div id="colors" className="h-full w-full flex flex-row place-content-center" style={{backgroundColor:'rgb('+this.state.rgb_array[0]+', '+this.state.rgb_array[1]+', '+this.state.rgb_array[2]+')'}} onClick={this.get_random_color}></div>
+                <div id="colors" className="h-full w-full flex flex-row place-content-center" >
+                    {this.state.colors_id.map(color => <div key={color} onClick={()=>this.get_random_color(color)} id="color_color" class="p-0 m-0 h-full" style={{backgroundColor:'rgb('+this.state.rgb_array[color][0]+', '+this.state.rgb_array[color][1]+', '+this.state.rgb_array[color][2]+')', 'width':'100%'}}> </div>) }
+                </div>
             </div>
         )
     }
